0d7fc4bb98b0ebbee9d9635304539e9fbf2171f0
Jackson Tian <shyvo1987@gmail.com>
2020-07-17 10:29:03 +0800

    rename tea to dara

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,14 @@ import {
 } from 'vscode';
 import { completions } from './completions';
 
-class TeaCompletionItemProvider implements CompletionItemProvider {
+class DaraCompletionItemProvider implements CompletionItemProvider {
 	provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext): ProviderResult<CompletionItem[] | CompletionList> {
 		return completions;
 	}
 }
 
 export function activate(context: ExtensionContext) {
-	context.subscriptions.push(languages.registerCompletionItemProvider('tea', new TeaCompletionItemProvider(), '.', '\"'));
+	context.subscriptions.push(languages.registerCompletionItemProvider('dara', new DaraCompletionItemProvider(), '.', '\"'));
 }
 
 export function deactivate(): Thenable<void> | undefined {
